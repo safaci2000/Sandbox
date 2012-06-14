ce9837e885855d4ed8d4d9f8e40a2f7f5df4ad4a
Samir Faci <samir.faci@nextag.com>
2012-06-14 10:51:25 -0700

    Build Map Done, working on serializing map of Maps into usable content.

--- src/main/java/com/esamir/Main.java
+++ src/main/java/com/esamir/Main.java
@@ -6,7 +6,10 @@ import org.apache.log4j.Logger;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 
-import java.util.*;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 
 /**
  * User: sfaci
@@ -14,113 +17,161 @@ import java.util.*;
  * Time: 12:26 PM
  */
 public class Main {
-    static Logger logger = Logger.getLogger(Main.class);
-    static JSONArray jsonArray = new JSONArray();
-    static private final String objectName = Seller.class.getName();
+	static Logger logger = Logger.getLogger(Main.class);
+	static JSONArray jsonArray = new JSONArray();
+	static private final String objectName = Seller.class.getName();
 
 
-    static {
-        BasicConfigurator.resetConfiguration();
-        BasicConfigurator.configure();
-    }
+	static {
+		BasicConfigurator.resetConfiguration();
+		BasicConfigurator.configure();
+	}
 
-    static private String getCompositeKey(Object key) {
-        return key + ":" + objectName;
-    }
-
-
-    public static Object put2(Object key, Object value) {
-        if (key instanceof Long) {
-            Map<String, Object> xcacheObjectData = new HashMap<String, Object>();
-            if (value instanceof Collection) {
-                logger.debug("found a collection and I'll fix it up like a collection");
-                Iterator<XCacheComplexObject> xcacheObjectsIter = ((Collection<XCacheComplexObject>) value).iterator();
-                JSONObject mapper = new JSONObject();
-                while (xcacheObjectsIter.hasNext()) {
-                    XCacheComplexObject xcacheComplexDataObject = xcacheObjectsIter.next();
-                    xcacheObjectData.put(xcacheComplexDataObject.getCacheId(), xcacheComplexDataObject.serialize(mapper));
-                    mapper.clear();
-                }
-            } else if (value instanceof Map) {
-                logger.debug("found a HashMap of ISellerProgramRecords");
-                Map values = (Map) value;
-                if (values.keySet().size() == 0) {
-                    return null;
-                }
-                Set keys = values.keySet();
-                Iterator it = keys.iterator();
-                Object map_key = it.next();
-                Object a_value = values.get(map_key);
-                if (a_value instanceof Map || a_value instanceof Collection) {
-                    //Call yourself.
-                }
-
-                logger.debug("Hash Map is keyed by: " + map_key.getClass().getName());
-                logger.debug("Value type is: " + a_value.getClass().getName());
+	static private String getCompositeKey(Object key) {
+		return key + ":" + objectName;
+	}
 
 
-            } else {
-                logger.debug(value.getClass().getName());
-                logger.debug("I did not find an XCache Object nor a collection");
-            }
-            final String compositeKey = getCompositeKey(key);
-            //xCacheCassandraManager.updateRow(compositeKey, xcacheObjectData);
-            logger.info("Inserted " + objectName + " into Cassandra: " + compositeKey);
+	public static Object put2(Object key, Object value) {
 
-            return value;
-        } else {
-            return null;
-        }
-    }
+		if (key instanceof Long) {
+			Map<String, Object> xcacheObjectData = new HashMap<String, Object>();
 
-    public static Map buildMapInstance() {
-        Utility util = Utility.getInstance();
-        Map parent_map = new HashMap();
+			//Single case.
+			if (value instanceof XCacheObject) {
+				XCacheObject xcacheObject = (XCacheObject) value;
+				xcacheObject.populateCacheMap(xcacheObjectData);
+			} else if (value instanceof Collection) {
+				logger.debug("found a collection and I'll fix it up like a collection");
 
-        for (int i = 0; i < 5; i++) {
+				Iterator<XCacheComplexObject> xcacheObjectsIter = ((Collection<XCacheComplexObject>) value).iterator();
+				JSONObject mapper = new JSONObject();
 
-            Long key_l1 = util.getRandomLong();
-            Map a = new HashMap();
-            for (int y = 0; y < 5; y++) {
+				while (xcacheObjectsIter.hasNext()) {
+					XCacheComplexObject xcacheComplexDataObject = xcacheObjectsIter.next();
+					xcacheObjectData.put(xcacheComplexDataObject.getCacheId(), xcacheComplexDataObject.serialize(mapper));
+					mapper.clear();
+				}
+				return xcacheObjectData;
+			} else if (value instanceof Map) {
+				Map map_values = (Map) value;
 
-                Long key_l2 = util.getRandomLong();
+				if (map_values.keySet().size() == 0)
+					return null;
 
-                Map b = new HashMap();
-                a.put(key_l2, b);
-                for (int z = 0; z < 3; z++) {
-
-                    String key_l3 = util.getRandomString();
-                    Seller obj = Seller.getRandomInstance();
-                    if (obj instanceof XCacheComplexObject) {
-                        logger.info("Seller object matches XCacheComplexObj");
-                    }
-                    b.put(key_l3, obj);
-                    logger.debug("Key l3 is: " + key_l3);
-                }
-                logger.debug("key l2 is: " + key_l2);
-
-            }
-            parent_map.put(key_l1, a);
-
-        }
-
-        return parent_map;
-    }
+				JSONObject jsonMap = new JSONObject();
+				JSONArray jsonArray = new JSONArray();
 
 
-    public static void main(String[] args) {
-        Map serializeMe = buildMapInstance();
-        put2(2342L, serializeMe);
+				Iterator it = map_values.values().iterator();
+				Object item_value = it.next();
+
+				if ((item_value instanceof Map) || (item_value instanceof Collection)) {
+					// Call yourself.
+				} else if (item_value instanceof XCacheComplexObject) {
+					JSONObject mapper = new JSONObject();
+					xcacheObjectData.put(((XCacheComplexObject) item_value).getCacheId(), ((XCacheComplexObject) item_value).serialize(mapper));
+				}
+				logger.debug("Value type is: " + item_value.getClass().getName());
+
+			} else {
+				logger.debug(value.getClass().getName());
+				logger.debug("I did not find an XCache Object nor a collection");
+			}
+
+			final String compositeKey = getCompositeKey(key);
+
+			// xCacheCassandraManager.updateRow(compositeKey, xcacheObjectData);
+			logger.info("Inserted " + objectName + " into Cassandra: " + compositeKey);
+
+			return xcacheObjectData;
+		} else {
+			return null;
+		}
+	}
+
+	public static JSONObject serializeComplex(Object value) {
+		if (value instanceof Map) {
+			Map map_value = (Map) value;
+			JSONArray jsonarray = new JSONArray();
+			JSONObject jsonObject = new JSONObject();
+			Iterator it = map_value.values().iterator();
+			Object item_value = it.next();
+			if (item_value instanceof XCacheComplexObject)  // Simple? Complex Object
+			{
+				logger.info(((XCacheComplexObject) item_value).serialize(new JSONObject()));
+
+			} else if (item_value instanceof Map) {  // HashMap
+				serializeComplex(item_value);
+
+			} else if (item_value instanceof Collection) {  //ie. List.
+
+			}
 
 
-        //build
-        for (int i = 0; i < 10; i++) {
-            Double a = Math.pow(i, 2.0);
+		} else if (value instanceof XCacheComplexObject) {
+			XCacheComplexObject complex = (XCacheComplexObject) value;
+			JSONObject jsonvalue = new JSONObject();
+			String payload = complex.serialize(jsonvalue);
+			return jsonvalue;
 
-        }
-        logger.info("woot");
+		} else {
+			logger.error("Unsupported key detected");
+		}
+
+		return null;
+	}
+
+	public static Map buildMapInstance() {
+		Utility util = Utility.getInstance();
+		Map parent_map = new HashMap();
+
+		for (int i = 0; i < 5; i++) {
+
+			Long key_l1 = util.getRandomLong();
+			Map a = new HashMap();
+			for (int y = 0; y < 5; y++) {
+
+				Long key_l2 = util.getRandomLong();
+
+				Map b = new HashMap();
+				a.put(key_l2, b);
+				for (int z = 0; z < 3; z++) {
+
+					String key_l3 = util.getRandomString();
+					Seller obj = Seller.getRandomInstance();
+					b.put(key_l3, obj);
+				}
+
+			}
+			parent_map.put(key_l1, a);
+
+		}
+
+		return parent_map;
+	}
 
 
-    }
+	public static void main(String[] args) {
+		Map serializeMe = buildMapInstance();
+		Map simpleMap = new HashMap();
+		simpleMap.put(1, Seller.getRandomInstance());
+		simpleMap.put(2, Seller.getRandomInstance());
+		simpleMap.put(3, Seller.getRandomInstance());
+		simpleMap.put(4, Seller.getRandomInstance());
+		simpleMap.put(5, Seller.getRandomInstance());
+
+		serializeComplex(simpleMap);
+
+
+		//build
+		for (int i = 0; i < 10; i++) {
+			Double a = Math.pow(i, 2.0);
+
+		}
+		logger.info("woot");
+
+
+	}
 
 }
---
+++ src/main/java/com/esamir/XCacheObject.java
@@ -0,0 +1,8 @@
+package com.esamir;
+
+import java.util.Map;
+
+public interface XCacheObject {
+
+	public void populateCacheMap(Map<String, Object> populatedData);
+}
